e6ba4cd50c4910c459d04737a312b9926273b763
bybit0408 <bybit0408@gmail.com>
2021-08-19 14:52:49 +0900

    add weather

--- chromeApp/js/weather.js
+++ chromeApp/js/weather.js
@@ -1,8 +1,18 @@
 
+const API_KEY = "feb5c7a1bc001fc0e8b30e40c501888d";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`You live in ${lat} ${lng}`);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-Child");
+        const city = document.querySelector("#weather span:last-Child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}C`;
+    });
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.");
